013577c83586d2f59b1f265bb0222669945be979
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 12:10:08 -0300

    feat: entrada CLI mínima
    intent: feature

--- obj/Debug/net8.0/api_rest_de_health_check_566f2c.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_rest_de_health_check_566f2c.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_rest_de_health_check_566f2c")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883a11d48a0fbc2ab7332bec83db2319b7f184a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ea4a9f786e85dbc6a64911554bb26c92594976")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_rest_de_health_check_566f2c")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_rest_de_health_check_566f2c")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
